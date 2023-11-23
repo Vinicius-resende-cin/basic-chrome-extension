6dd28dba210ceea43b6e6f225c435f48900b567b
Vinicius Resende <92769975+Vinicius-resende-cin@users.noreply.github.com>
2023-11-23 18:53:13 -0300

    Change content file to modigy comments

--- src/scripts/content.ts
+++ src/scripts/content.ts
@@ -1,16 +1,39 @@
-// Function to generate a random compliment
-function generateCompliment(): string {
-  const compliments = [
-    "You're doing great!",
-    "You're awesome!",
-    "You're a superstar!",
-    "You're amazing!",
-    "You're killing it!"
-  ];
-
-  const randomIndex = Math.floor(Math.random() * compliments.length);
-  return compliments[randomIndex];
+function changeText(text: string, comment: HTMLElement) {
+  /**
+   * Change a comments's text
+   */
+  comment.innerText = text;
 }
 
-// Send an alert with a compliment
-alert(generateCompliment());
+function changeComments(newText: string) {
+  /**
+   * Add a text in the app's comments
+   */
+  // get all comments
+  const comments = document.querySelectorAll(
+    "div.js-comments-holder"
+  ) as NodeListOf<HTMLDivElement>;
+
+  if (comments.length === 0) return;
+
+  // loop through comments to find the app's comments
+  for (let i = 0; i < comments.length; i++) {
+    const comment = comments[i];
+
+    // get the comment's author
+    const authorTitle = comment.querySelector(
+      "strong a.author.Link--primary.text-bold.css-overflow-wrap-anywhere"
+    ) as HTMLAnchorElement;
+    const authorName = authorTitle.innerText;
+
+    if (authorName !== "basic-reviewer") continue;
+
+    // change the comment's text
+    const commentBody = comment.querySelector(
+      "div.comment-body.markdown-body.js-comment-body.soft-wrap.css-overflow-wrap-anywhere.user-select-contain.d-block p"
+    ) as HTMLParagraphElement;
+    changeText(newText, commentBody);
+  }
+}
+
+changeComments("This is a test comment");
