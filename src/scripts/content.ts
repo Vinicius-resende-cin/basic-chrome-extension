48bda4d4929639ee0425e09140e48ec00be15469
Vinicius Resende <92769975+Vinicius-resende-cin@users.noreply.github.com>
2023-12-02 03:04:04 -0300

    Change content to directly modify revision text

--- src/scripts/content.ts
+++ src/scripts/content.ts
@@ -1,39 +1,60 @@
-function changeText(text: string, comment: HTMLElement) {
-  /**
-   * Change a comments's text
-   */
-  comment.innerText = text;
+function getAllFileDiffs() {
+  return document.querySelectorAll("div[id^=diff-]");
 }
 
-function changeComments(newText: string) {
-  /**
-   * Add a text in the app's comments
-   */
-  // get all comments
-  const comments = document.querySelectorAll(
-    "div.js-comments-holder"
-  ) as NodeListOf<HTMLDivElement>;
-
-  if (comments.length === 0) return;
-
-  // loop through comments to find the app's comments
-  for (let i = 0; i < comments.length; i++) {
-    const comment = comments[i];
-
-    // get the comment's author
-    const authorTitle = comment.querySelector(
-      "strong a.author.Link--primary.text-bold.css-overflow-wrap-anywhere"
-    ) as HTMLAnchorElement;
-    const authorName = authorTitle.innerText;
-
-    if (authorName !== "basic-reviewer") continue;
-
-    // change the comment's text
-    const commentBody = comment.querySelector(
-      "div.comment-body.markdown-body.js-comment-body.soft-wrap.css-overflow-wrap-anywhere.user-select-contain.d-block p"
-    ) as HTMLParagraphElement;
-    changeText(newText, commentBody);
+function getFileDiff(filePath: string) {
+  for (let fileDiff of getAllFileDiffs()) {
+    if (fileDiff.getAttribute("data-tagsearch-path") === filePath) {
+      return fileDiff;
+    }
   }
+  return null;
 }
 
-changeComments("This is a test comment");
+function getDiffLine(fileDiff: Element, lineNumber: number) {
+  return fileDiff.querySelector(`tr:has(td[data-line-number="${lineNumber}"])`);
+}
+
+function changeLineText(line: Element, text: string) {
+  const lineBody = line.querySelector("span[data-code-marker='+']");
+  if (lineBody === null) {
+    throw new Error(
+      `[EXT_ERROR] Could not find the line body in the line ${line.getAttribute(
+        "data-line-number"
+      )}`
+    );
+  }
+
+  const lineText = lineBody.querySelector("span");
+  if (lineText === null) {
+    throw new Error(
+      `[EXT_ERROR] Could not find the line text in the line ${line.getAttribute(
+        "data-line-number"
+      )}`
+    );
+  }
+
+  lineText.textContent = text;
+}
+
+//TODO: make api calls to the github app so that the file and the line number can be passed in
+function main() {
+  const fileDiff = getFileDiff("src/main/java/Text.java");
+  if (fileDiff === null) {
+    console.log("[EXT_WARNING] No diff found in this page");
+    return;
+  }
+
+  const line = getDiffLine(fileDiff, 33);
+  if (line === null) {
+    throw new Error(
+      `[EXT_ERROR] Could not find a modified line in the diff ${fileDiff.getAttribute(
+        "data-tagsearch-path"
+      )}`
+    );
+  }
+
+  changeLineText(line, "Hello World!");
+}
+
+main();
