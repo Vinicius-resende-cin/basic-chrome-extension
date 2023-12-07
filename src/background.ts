e8a06514028174b48669161427b940e90ff9da98
Vinicius Resende <92769975+Vinicius-resende-cin@users.noreply.github.com>
2023-12-07 05:59:32 -0300

    Change background to work with tab update

--- src/background.ts
+++ src/background.ts
@@ -1,11 +1,17 @@
-chrome.webNavigation.onHistoryStateUpdated.addListener(
-  (details) => {
-    console.log("Listener called at: " + details.url);
+chrome.tabs.onUpdated.addListener((tabId, changeinfo, tab) => {
+  // Guarantee that the tab is fully loaded
+  if (changeinfo.status !== "complete") return;
 
-    chrome.scripting.executeScript({
-      target: { tabId: details.tabId },
-      files: ["./content.js"]
-    });
-  },
-  { url: [{ urlMatches: "^https:\\/\\/github\\.com\\/.+\\/.+\\/pull\\/\\d+\\/files$" }] }
-);
+  // Filter the url to only run on PR files page
+  if (tab.url === undefined) throw new Error("tab.url is undefined");
+  const urlMatch = tab.url.match(/^https:\/\/github\.com\/.+\/.+\/pull\/\d+\/files$/);
+  if (urlMatch === null) return;
+
+  console.log("Listener called at: " + tab.url);
+
+  // Execute the content script
+  chrome.scripting.executeScript({
+    target: { tabId: tabId },
+    files: ["./content.js"]
+  });
+});
