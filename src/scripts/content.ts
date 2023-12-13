f06680e14e21631e1631e3e6affc68b760ea89e2
Vinicius Resende <92769975+Vinicius-resende-cin@users.noreply.github.com>
2023-12-12 23:23:32 -0300

    Change content to insert popups instead of arrows

--- src/scripts/content.ts
+++ src/scripts/content.ts
@@ -1,5 +1,5 @@
 import * as githubDOM from "../utils/githubDOMUtils.js";
-import * as githubArrow from "../utils/githubDiffArrowUtils.js";
+import { insertPopup } from "../utils/popup.js";
 
 //TODO: make api calls to the github app so that the file and the line number can be passed in
 function main() {
@@ -27,7 +27,40 @@ function main() {
     );
   }
 
-  githubArrow.drawArrow(fileDiff, fromLineIndex, toLineIndex, lineList.length);
+  const fromLine = lineList[fromLineIndex];
+  const toLine = lineList[toLineIndex];
+
+  const fromLineText = fromLine.querySelector("td.js-file-line") as HTMLTableCellElement;
+  const toLineText = toLine.querySelector("td.js-file-line") as HTMLTableCellElement;
+  if (fromLineText === null || toLineText === null) {
+    throw new Error(
+      `[EXT_ERROR] Could not find the line text in the diff ${fileDiff.getAttribute(
+        "data-tagsearch-path"
+      )}`
+    );
+  }
+
+  const fromLineNumberMatch = fromLine.children[1].id.match(`${fileDiff.id}R(\\d+)`);
+  const toLineNumberMatch = toLine.children[1].id.match(`${fileDiff.id}R(\\d+)`);
+  if (fromLineNumberMatch === null || toLineNumberMatch === null) {
+    throw new Error(
+      `[EXT_ERROR] Could not find the line number in the diff ${fileDiff.getAttribute(
+        "data-tagsearch-path"
+      )}`
+    );
+  }
+
+  const fromLineNumber = parseInt(fromLineNumberMatch[1]);
+  const toLineNumber = parseInt(toLineNumberMatch[1]);
+
+  insertPopup(
+    fromLineText,
+    `This is the source for line <a href='#${toLineNumberMatch[0]}'>${toLineNumber}</a>`
+  );
+  insertPopup(
+    toLineText,
+    `This is the sink from line <a href='#${fromLineNumberMatch[0]}'>${fromLineNumber}</a>`
+  );
 }
 
 main();
---
+++ src/utils/popup.ts
@@ -0,0 +1,53 @@
+function createPopup(popupHTML: string) {
+  const separator = document.createElement("span");
+  separator.style.padding = "0 8px 0 0";
+
+  const popUpContainer = document.createElement("span");
+  popUpContainer.style.border = "1px solid black";
+  popUpContainer.style.position = "relative";
+  popUpContainer.style.display = "table-cell";
+  popUpContainer.style.width = "10%";
+  popUpContainer.style.padding = "0";
+  popUpContainer.style.border = "0";
+
+  const popoverButton = document.createElement("button");
+  popoverButton.innerText = "Alert";
+  popoverButton.style.width = "100%";
+  popoverButton.style.padding = "0";
+  popoverButton.style.border = "0";
+  popoverButton.style.fontSize = "10px";
+  popoverButton.style.backgroundColor = "red";
+
+  const popover = document.createElement("div");
+  popover.style.inset = "unset";
+  popover.style.position = "absolute";
+  popover.style.margin = "0px";
+  popover.style.padding = "0 5px 0 5px";
+  popover.style.width = "fit-content";
+  popover.style.right = "100%";
+  popover.style.top = "50%";
+  popover.style.transform = "translateY(-50%)";
+  popover.style.visibility = "hidden";
+  popover.innerHTML = popupHTML;
+  popover.style.whiteSpace = "nowrap";
+  popover.style.border = "1px solid black";
+  popover.style.color = "black";
+  popover.style.backgroundColor = "#ffe785";
+
+  popUpContainer.appendChild(separator);
+  popUpContainer.appendChild(popoverButton);
+  popUpContainer.appendChild(popover);
+
+  return { popUpContainer, popoverButton, popover };
+}
+
+export function insertPopup(element: Element, popupHTML: string) {
+  const { popUpContainer, popoverButton, popover } = createPopup(popupHTML);
+
+  element.appendChild(popUpContainer);
+
+  popoverButton.addEventListener("click", () => {
+    const newVisibility = popover.style.visibility === "visible" ? "hidden" : "visible";
+    popover.style.visibility = newVisibility;
+  });
+}
